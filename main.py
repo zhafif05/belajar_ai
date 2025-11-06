2863926063a6621a83340b712fb6b065710e6ead
zhafif05 <zhafifriz05@gmail.com>
2025-11-06 10:40:42 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,36 @@
+# # usia = 10
+# #
+# #exponen / pangkat 
+# # a ** b =
+# modulus / sisa bagi 
+# a % b
+# a // b  
+# 
+# # print('halo ' + str(usia))
+# #print('halo')
+
+# # input_nama = input('nama') #input bertipe data str
+# # print(input_nama)
+# #
+# # saldo_awal = input('masukkan saldo awal ')
+# # deposit = input('berapa deposite mu ')
+# #
+# # c = int(saldo_awal) + int(deposit)
+# # print('total hasil nya adalah ' + str(c) )
+# #
+# # pacar_saya = ['saya', 'kamu', 'aku']
+# #
+# #  for pacar in pacar_saya:
+# #      print(pacar)
+
+# saldo_awal= 5000
+# deposit= input('masukkan uang ? ')
+
+# saldo_total = int(saldo_awal) + int(deposit)
+# hutang = 50_000
+
+# if saldo_total >= hutang:
+#     saldo_akhir = saldo_total - hutang
+#     print('hutang telah lunas dan sisa uang anda adalah ' + str(saldo_akhir) )
+# else:
+# print('belum cukup untuk membayar hutang')
